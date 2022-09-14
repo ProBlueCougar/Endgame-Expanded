b732b35e75d873d7fa6340d69d1e9b9e1a00a711
BlueCougar <supermariolink11@gmail.com>
2022-09-14 17:04:51 -0600

    Initial commit

---
+++ src/main/java/net/probluecougar/endgameexpanded/EndgameExpanded.java
@@ -0,0 +1,16 @@
+package net.probluecougar.endgameexpanded;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EndgameExpanded implements ModInitializer {
+
+
+	public static final String MOD_ID = "endgameexpanded";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/probluecougar/endgameexpanded/EndgameExpandedClient.java
@@ -0,0 +1,10 @@
+package net.probluecougar.endgameexpanded;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EndgameExpandedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
